308c176c1d594cff0bb33617012bc8612ca0b082
Krako-de-Quanto <mails2016@nurfuerspam.de>
2016-08-01 13:26:29 +0200

    Starting to move code to specific classes to make the code more readable and to make it easier to work together with each other.

--- EU-Module-1/Program.cs
+++ EU-Module-1/Program.cs
@@ -34,7 +34,7 @@ namespace eCTD_indexer
         {            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
